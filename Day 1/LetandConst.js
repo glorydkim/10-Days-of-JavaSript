d9f5d2b0ac8735f2d514adb0fb8d53cffdcb1964
Glory Kim <glorydkim@gmail.com>
2017-10-15 22:18:19 -0400

    Update LetandConst.js

--- Day 1/LetandConst.js
+++ Day 1/LetandConst.js
@@ -81,14 +81,20 @@ function readLine() {
 
 
 function main() {
+    
+function main() {
     // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    
+    const PI = Math.PI;
+    var r = + readLine();
     
     // Print the area of the circle:
     
+    console.log(r * r * Math.PI)
+    
     // Print the perimeter of the circle:
-   
-   
-   
+    
+    console.log(2 * Math.PI * r);
    
        try {    
         // Attempt to redefine the value of constant variable PI
